02c6c59697babfe1a569479131e01a29cea25e8d
Benoit Foujols <benoit@foujols.com>
2022-04-04 15:40:12 +0200

    (fix) correction du varDump sur le buffer

--- src/DebugHandler.php
+++ src/DebugHandler.php
@@ -4,12 +4,10 @@ namespace Quizz;
 
 class DebugHandler
 {
-    public static function dump($input): bool|string
+    public static function dump($input)
     {
-        ob_start();
         echo "<pre>";
         var_dump($input);
         echo "</pre>";
-        return ob_get_clean();
     }
 }
