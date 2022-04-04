0742d590ef3eaafc8445b3e747b199245bca7c02
Benoit Foujols <benoit@foujols.com>
2022-04-04 20:54:39 +0200

    (fix) correction du varDump sur le buffer

--- src/DebugHandler.php
+++ src/DebugHandler.php
@@ -6,8 +6,10 @@ class DebugHandler
 {
     public static function dump($input)
     {
+        //ob_start();
         echo "<pre>";
         var_dump($input);
         echo "</pre>";
+        //return ob_get_clean();
     }
 }
